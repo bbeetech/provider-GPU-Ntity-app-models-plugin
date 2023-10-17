5b2f65a97482231484dc12eab4af6f992016ba52
Kyle Nguyen <kylenyan1215@gmail.com>
2023-10-18 05:01:20 +0700

    chore: update bat file

--- fooocus/run.bat
+++ fooocus/run.bat
@@ -1,19 +1,5 @@
 @echo off
 
-set ENV_NAME=fooocus
-
-rem Check if the environment already exists
-conda env list | find /i "%ENV_NAME%" > nul
-if errorlevel 1 (
-    echo Creating Conda environment: %ENV_NAME%
-    conda env create -f environment.yaml
-) else (
-    echo Conda environment %ENV_NAME% already exists. Skipping creation.
-)
-
-rem Activate the environment
-call conda activate %ENV_NAME%
-
 pip install pygit2==1.12.2
 pip install -r requirements_versions.txt
 python -s entry_with_update.py
