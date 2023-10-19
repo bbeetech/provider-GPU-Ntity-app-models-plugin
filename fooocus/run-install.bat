59f2ca896feec86a5113d06c0accc4557459855f
Kyle Nguyen <kylenyan1215@gmail.com>
2023-10-20 05:45:47 +0700

    chore: update install

--- fooocus/run-install.bat
+++ fooocus/run-install.bat
@@ -10,31 +10,31 @@ set "run_script=.\run.bat"
 rem Step 1: Download fooocus.7z with progress
 if not exist "%downloaded_file%" (
     choco install curl -y
-    echo Downloading %downloaded_file%...
+    echo Downloading "%downloaded_file%" ...
     curl -# -L -o "%downloaded_file%" "%download_url%"
 ) else (
-    echo %downloaded_file% already exists. Skipping download.
+    echo "%downloaded_file%" already exists. Skipping download.
 )
 
 rem Check if WinRAR is installed
 if not exist "%installDir%\WinRAR.exe" (
-    echo WinRAR is not installed in %installDir%. Please install it first.
+    echo WinRAR is not installed in "%installDir%". Please install it first.
     pause
     exit /b
 )
 
 rem Extract the downloaded archive to the installation directory
 if not exist "%extracted_folder%" (
-    echo Extracting %downloaded_file% to %extracted_folder% ...
-    "%installDir%\WinRAR.exe" x -ibck -idc -y %downloaded_file%
+    echo Extracting "%downloaded_file%" to "%extracted_folder%" ...
+    "%installDir%\WinRAR.exe" x -ibck -idc -y "%downloaded_file%"
 )
 
 rem Step 3: Run the run.bat script in the fooocus folder if it exists
 if exist "%extracted_folder%" (
-    echo Running %run_script% in %extracted_folder% ...
-    call %run_script%
+    echo Running "%run_script%" in "%extracted_folder%" ...
+    call "%extracted_folder%\%run_script%"
 ) else (
-    echo %extracted_folder% does not exist. Cannot run %run_script%.
+    echo "%extracted_folder%" does not exist. Cannot run "%run_script%".
 )
 
 endlocal
